64aae86b5a9685245f4edec030f023702321d9b7
lynxz <lynxz8866@gmail.com>
2022-03-30 21:22:06 +0800

    101. 对称二叉树

--- src/Solution.java
+++ src/Solution.java
@@ -1296,5 +1296,36 @@ public class Solution {
         return Math.max(depthL, depthR);
     }
 
+    /**
+     * https://leetcode-cn.com/problems/symmetric-tree/
+     * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+     * 示例 1：
+     * 输入：root = [1,2,2,3,4,4,3]
+     * 输出：true
+     * <p>
+     * 题解:
+     * 1. rootNode 为空,则自然是对称的
+     * 2. rootNode 非空,则要求其左子树与右子树对称
+     * -        a. 左右子树根节点值一致
+     * -        b. 左子树的左节点与右子树的右节点一致
+     * -        c. 左子树的右节点与右子树的左节点一致
+     * 以上,即可得出递归解法
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
 
 }
