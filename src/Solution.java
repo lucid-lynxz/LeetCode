8963c2348d9a511ca22f59a588e8bef239b4cc52
lynxz <lynxz8866@gmail.com>
2022-03-30 21:38:57 +0800

    111. 二叉树的最小深度

--- src/Solution.java
+++ src/Solution.java
@@ -1327,5 +1327,42 @@ public class Solution {
         return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 
+    /**
+     * 111. 二叉树的最小深度
+     * 给定一个二叉树，找出其最小深度。
+     * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+     * 说明：叶子节点是指没有子节点的节点。
+     * 示例 1：
+     * 输入：root = [3,9,20,null,null,15,7]
+     * 输出：2
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) { // 空树, 直接返回0
+            return 0;
+        }
+        return minDepth(root, 1);
+    }
+
+    /**
+     * 递归直到叶子节点
+     * 叶子节点: node!=null && node.left==null && node.right==null
+     */
+    private int minDepth(TreeNode node, int depth) {
+        // 若node节点不存在,则其并非叶子节点,由于需要获取最小节点树,因此直接返回最大值
+        if (node == null) {
+            return Integer.MAX_VALUE;
+        }
+
+        // 当前已是叶子节点,则返回深度
+        if (node.left == null && node.right == null) {
+            return depth;
+        }
+
+        // 当前非叶子节点, 则递归子节点, 直到叶子节点
+        return Math.min(minDepth(node.left, depth + 1), minDepth(node.right, depth + 1));
+    }
 
 }
