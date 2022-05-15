4529495872b2d6e55a6cec2b3c02f9ce9efa8c62
lynxz <lynxz8866@gmail.com>
2022-05-15 18:47:37 +0800

    面试题 16.17. 连续数列

--- src/Solution.java
+++ src/Solution.java
@@ -889,15 +889,16 @@ public class Solution {
 
     /**
      * 53. 最大子数组和
+     * https://leetcode-cn.com/problems/maximum-subarray
+     * <p>
+     * 面试题 16.17. 连续数列
+     * https://leetcode.cn/problems/contiguous-sequence-lcci/
      * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
      * 子数组 是数组中的一个连续部分。
      * 示例 1：
      * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
      * 输出：6
      * 解释：连续子数组[4,-1,2,1] 的和最大，为6
-     * 来源：力扣（LeetCode）
-     * 链接：https://leetcode-cn.com/problems/maximum-subarray
-     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     public int maxSubArray(Integer[] nums) {
         // 动态规划
